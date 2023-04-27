7479ceea19c9051bdba6118e0d6d88964f0bb373
Sakib <shadmanshakib1@gmail.com>
2023-04-27 16:29:59 +0600

    updated secondary url

--- barikoitrace/src/main/java/com/barikoi/barikoitrace/network/Api.java
+++ barikoitrace/src/main/java/com/barikoi/barikoitrace/network/Api.java
@@ -3,7 +3,7 @@ package com.barikoi.barikoitrace.network;
 
 public class Api {
     public static final String base_url="https://trace.barikoi.xyz/api/v1/business";
-    public static final String base_url_2=" https://backend.barikoi.com:8888/api/v1/";
+    public static final String base_url_2="https://trace.barikoi.xyz/api/v1/";
 
     public static final String start_trip_url=base_url+"/start_trip";
     public static final String end_trip_url=base_url+"/end_trip";
