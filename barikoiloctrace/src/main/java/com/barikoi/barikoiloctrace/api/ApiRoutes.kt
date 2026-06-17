967ce5599a3e765267c63f68c9d4375cd7fb5295
Miraz <mmrrahman508@gmail.com>
2026-06-17 12:53:41 +0600

    remove unused API route constants from ApiRoutes.kt

--- barikoiloctrace/src/main/java/com/barikoi/barikoiloctrace/api/ApiRoutes.kt
+++ barikoiloctrace/src/main/java/com/barikoi/barikoiloctrace/api/ApiRoutes.kt
@@ -5,13 +5,5 @@ object ApiRoutes {
     const val MQTT_URL = "tcp://mqtt.bmapsbd.com:1883"
 
     const val AUTHENTICATE = "/sdk/authenticate"
-    const val ADD_GPX = "/sdk/add-gpx"
-    const val BULK_GPX = "/sdk/bulk-gpx"
-    const val USER = "/sdk/user"
-    const val START_TRIP = "/trip/create"
-    const val END_TRIP = "/trip/end"
-    const val TRIP_SYNC = "/trip/offline"
-    const val ACTIVE_TRIP = "/trip/check-active-trip"
     const val COMPANY_SETTINGS = "/sdk/company/settings"
-    const val APP_LOG = "/app/log"
 }
