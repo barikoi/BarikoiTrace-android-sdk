8ea278b95af5d1519a0d7a6b405a46439350eb2e
Miraz <mmrrahman508@gmail.com>
2026-07-27 18:19:52 +0600

    remove: deprecate BarikoiTrace module and related classes, including Api, ApiRequestManager, ApplicationBinder, and others

--- barikoitrace/src/main/java/com/barikoi/barikoitrace/callback/BarikoiTraceBulkUpdateCallback.java
+++
@@ -1,9 +0,0 @@
-package com.barikoi.barikoitrace.callback;
-
-
-import com.barikoi.barikoitrace.models.BarikoiTraceError;
-
-public interface BarikoiTraceBulkUpdateCallback {
-    void onBulkUpdate();
-    void onFailure(BarikoiTraceError barikoiError);
-}
--- barikoitrace/src/main/java/com/barikoi/barikoitrace/exceptions/ContextException.java
+++
@@ -1,8 +0,0 @@
-package com.barikoi.barikoitrace.exceptions;
-
-
-public class ContextException extends RuntimeException {
-    public ContextException(String str) {
-        super(str);
-    }
-}
