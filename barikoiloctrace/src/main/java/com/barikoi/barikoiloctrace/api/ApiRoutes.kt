34647e8c4410fd4f9915778811a3419aa0f5f936
Miraz <mmrrahman508@gmail.com>
2026-07-27 10:31:14 +0600

    update: modify BASE_URL and MQTT_URL in ApiRoutes to use new trace.bmapsbd.com endpoints

--- barikoiloctrace/src/main/java/com/barikoi/barikoiloctrace/api/ApiRoutes.kt
+++ barikoiloctrace/src/main/java/com/barikoi/barikoiloctrace/api/ApiRoutes.kt
@@ -1,8 +1,8 @@
 package com.barikoi.barikoiloctrace.api
 
 object ApiRoutes {
-    const val BASE_URL = "https://api.mqtt.bmapsbd.com/api/v1/"
-    const val MQTT_URL = "tcp://mqtt.bmapsbd.com:1883"
+    const val BASE_URL = "https://api.trace.bmapsbd.com/api/v1/"
+    const val MQTT_URL = "tcp://broker.trace.bmapsbd.com:1883"
 
     const val AUTHENTICATE = "/sdk/authenticate"
     const val COMPANY_SETTINGS = "/sdk/company/settings"
